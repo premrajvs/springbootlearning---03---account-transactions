068c7fca209a6bdea414547a55a2f78aebea4956
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-11 14:07:00 -0600

    init

---
+++ src/main/java/com/performanceengineering/customertransactions/CustomerTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.performanceengineering.customertransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTransactionsApplication.class, args);
+	}
+
+}
